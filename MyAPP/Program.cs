d7082b1d8bf3963b863057881bb7d8ef55cbbd90
FabSyntaxDev <fabio.dsm.eng.software@gmail.com>
2026-07-02 21:12:21 -0300

    para descobrir o maximo do valor de uma int,

--- MyAPP/Program.cs
+++ MyAPP/Program.cs
@@ -11,11 +11,13 @@ namespace MyAPP
             int y = 99999999;
             long l = 99999999999L;
 
+            int t = int.MaxValue;
 
             
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(l);
+            Console.WriteLine(t);
         }
 }
 }
