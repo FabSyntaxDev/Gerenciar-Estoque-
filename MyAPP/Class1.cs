c687894ad360952e52cb19d8feea4006ea87b639
FabSyntaxDev <fabio.dsm.eng.software@gmail.com>
2026-07-03 22:49:51 -0300

    primeiro commit

--- MyAPP/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyAPP
-{
-    internal class Class1
-    {
-        public int valor1 = 2;
-        public int valor2;
-    }
-}
--- MyAPP/Program.cs
+++ MyAPP/Program.cs
@@ -4,57 +4,21 @@ using User;
 
 namespace MyAPP
 {
-    class Program
+    class App
     {
         static void Main(string[] args)
         {
 
-            sbyte x = 100;
-            int y = 99999999;
-            long l = 99999999999L;
-            int t = int.MaxValue;
+            Produto nome, preco, quantidade;
 
-
-            Class1 Valor1, Valor2;
-
-            Valor1 = new Class1();
-            Valor2 = new Class1();
+            nome = new Produto();
+            preco = new Produto();
+            quantidade = new Produto();
 
 
 
-            Console.WriteLine($"valores: {Valor1.valor1}");
-
-            
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(l);
-            Console.WriteLine(t);
-
-
-            string nomeInput;
-
-            Console.WriteLine("Entre com as medidas do triangulo X");
-            nomeInput = Console.ReadLine();
-            Console.WriteLine(nomeInput);
-
-
-
-            Console.WriteLine("----------------------------------");
-
-            User11 nome, password, email;
-
-
-            string dados = new User11().dados();
-
-            nome = new User11();
-            password = new User11();
-            email = new User11();
-
-            Console.WriteLine("Digita seu nome");
-            nome.nome = Console.ReadLine();
-
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine(dados);
+            Console.WriteLine("Entre com os dados do Produto");
+            string nome = Console.ReadLine();
 
 
         }
