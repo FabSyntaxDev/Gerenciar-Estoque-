7e635540c428fb80f052cd21995e8e01f33cde95
FabSyntaxDev <fabio.dsm.eng.software@gmail.com>
2026-07-03 22:25:52 -0300

    Adiciona classe Produto com cálculo de estoque
    Cria o arquivo Produto.cs com a classe interna Produto no namespace MyAPP, incluindo campos privados para nome, preço e quantidade, além do método ValorTotalEmEstoque(). Inclui os namespaces necessários.

---
+++ MyAPP/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAPP
+{
+    internal class Produto
+    {
+
+        string nome;
+        double preco;
+        int quantidade;
+
+        public int ValorTotalEmEstoque()
+        {
+            int total = ((int)preco * quantidade);
+            return total;
+        }
+
+
+
+
+
+    }
+}
